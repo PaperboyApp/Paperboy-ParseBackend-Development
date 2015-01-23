ecad0e85094b44f58f220c0683f498cf49bbf14e
Alvaro Serrano <marran51@gmail.com>
2015-01-23 12:26:02 -0300

    fixed users not being added to role User

--- cloud/main.js
+++ cloud/main.js
@@ -84,21 +84,18 @@ Parse.Cloud.define("getVerificationNumber", function(request, response) {
   var query = new Parse.Query(Parse.User)
   query.equalTo("username", phone)
 
-  query.first({
-    success: function(user) {
-      if (!user) {
-        user = createUser(phone)
-      }
-      Parse.Cloud.run("sendSMS", {
-        phone: phone,
-        body: "Your code: " + user.get('verificationNumber')
-      })
-      response.success()
-    },
-    error: function(error) {
-      response.error(error)
+  query.first().then(function (user) {
+    if (!user) {
+      user = createUser(phone)
     }
+    Parse.Cloud.run("sendSMS", {
+      phone: phone,
+      body: "Your code: " + user.get('verificationNumber')
+    })
+
+    return user
   })
+
 })
 
 Parse.Cloud.define("publish", function(request, response) {
@@ -145,6 +142,8 @@ Parse.Cloud.define("publish", function(request, response) {
   })
 })
 
+
+
 // Create user
 function createUser(userPhoneNumber) {
   // Generate Verification Number
@@ -158,15 +157,19 @@ function createUser(userPhoneNumber) {
   user.set('verified', false)
 
   // Sign up user
-  user.signUp(null).then(function(user) {
-    // Get user inside User role
-    var query = new Parse.Query(Parse.Role)
-    query.equalTo('name', 'User')
-    return query.first()
-  }).then(function (role) {
-    Parse.Cloud.useMasterKey()
-    role.getUsers().add(user)
-    role.save()
+  user.signUp(null).then(function (user) {
+    var roleQuery = new Parse.Query(Parse.Role)
+    console.log("role query")
+    roleQuery.get("bZVhujLFAN", {
+      success: function (role) {
+        console.log("saving to role " + role)
+        role.getUsers().add(user)
+        role.save()
+      },
+      error: function (error) {
+        console.log(error)
+      }
+    })
   })
 
   return user
